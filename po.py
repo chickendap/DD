2494a4d40a5ce9bb511e02ceb0ed41ff3d9a959e
chickendap <chergegus@gmail.com>
2026-01-24 15:22:27 +0700

    Add files via upload

--- po.py
+++ po.py
@@ -11,7 +11,20 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
 
-        
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height -80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height -80:
+            self.rect.y += self.speed 
+
 back =(200,255,255)
 win_width = 600
 win_height = 500
@@ -21,10 +34,47 @@ clock = time.Clock()
 fps = 60
 game = True
 
+racket1 = Player("hhhh",30,200,4,50,150)
+racket2 = Player("hhh",520,200,4,50,150)
+ball = GameSprite("nhhh",200,200,4,50,50)
+
+font.init()
+font = font.Font(None,35)
+lose1 = font.render('Player1 lose',True,(180,0,0))
+lose2 = font.render('Player2 lose',True,(180,0,0))
+
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    if finish != True:
+        window.fill(back)
+        racket1.update_1()
+        racket2.update_2()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2,ball):
+            speed_x *=-1
+            speed_x *=1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x <0:
+            finish = True
+            window.blit(lose1,(200,200))
+            game_over =True
+        
+        if ball.rect.x >win_width:
+            finsih =True
+            window.blit(lose2,(200,200))
+
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(fps)
